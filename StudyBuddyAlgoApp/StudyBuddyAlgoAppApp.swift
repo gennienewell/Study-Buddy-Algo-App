75f30ccf9efdef00fa84665939a1f8e72d1412d2
Gennie Newell <gennienewell@yahoo.com>
2022-10-14 20:19:54 -0400

    Initial Commit

---
+++ StudyBuddyAlgoApp/StudyBuddyAlgoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyBuddyAlgoAppApp.swift
+//  StudyBuddyAlgoApp
+//
+//  Created by Gennie Newell on 10/14/22.
+//
+
+import SwiftUI
+
+@main
+struct StudyBuddyAlgoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
