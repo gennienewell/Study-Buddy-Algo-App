5954839df32402f9a562a2fc9f8dbf91fb585e91
Gennie Newell <gennienewell@yahoo.com>
2022-10-16 19:55:40 -0400

    .Updated grid View

---
+++ StudyBuddyAlgoApp/Views/Components/FlashCardGrid.swift
@@ -0,0 +1,43 @@
+//
+//  FlashCardGrid.swift
+//  StudyBuddyAlgoApp
+//
+//  Created by Gennie Newell on 10/16/22.
+//
+
+import SwiftUI
+
+struct FlashCardGrid: View {
+    var flashCards: [FlashCard]
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("\(flashCards.count) \(flashCards.count > 1 ? "Flash Cards": "Flash Card" )")
+                    .font(.headline)
+                    .fontWeight(.medium)
+                .opacity(0.7)
+                
+                Spacer()
+                
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 160),spacing: 15)],spacing:15, content: {
+                    ForEach(flashCards){ fC in
+                        FlashCardSquare(flashCard: fC)
+                        
+                    }
+                })
+                                    
+                                    
+                                    
+                                    
+            }
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct FlashCardGrid_Previews: PreviewProvider {
+    static var previews: some View {
+        FlashCardGrid(flashCards: FlashCard.all)
+    }
+}
